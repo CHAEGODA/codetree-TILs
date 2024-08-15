a23019a901366afdd14d5c4c21e71508b429e9cf
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-08-15 19:05:14 +0900

    Title: 전화번호 바꾸기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240815/전화번호 바꾸기/change-phone-number.java
+++ 240815/전화번호 바꾸기/change-phone-number.java
@@ -7,7 +7,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        System.out.print("0" + a + "-" + c + "-" + b);
+        System.out.print("010"  + "-" + c + "-" + b);
 
     }
 }
