3be06e593e45d005de6ca7b67b229cda0b20cccb
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-10-01 15:14:16 +0900

    Title: 굉장한 숫자, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 241001/굉장한 숫자/amazing-number.java
+++ 241001/굉장한 숫자/amazing-number.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         if((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0))
-            System..out.println("true");
+            System.out.println("true");
         else
             System.out.println("false");    
     }
