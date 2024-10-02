8a98ac7a5cc2ec5df396d2e80d0e8dfbb0144cd8
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-10-02 15:22:30 +0900

    Title: 최댓값 구하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/최댓값 구하기/maximum-value.java
+++ 241002/최댓값 구하기/maximum-value.java
@@ -9,11 +9,15 @@ public class Main {
         b = sc.nextInt();
         c = sc.nextInt();
 
-        if(a >= b && a >= c)
-            System.out.println(a);
-        else if(b >= a && b >= c)
-            System.out.println(b);
-        else
-            System.out.println(c);
+        if(a >= b)
+            if(a >= c)
+                System.out.println(a);
+            else    
+                System.out.println(c);
+        else    
+            if(b >= c)
+                System.out.println(b);
+            else
+                System.out.println(c);
     }
 }
